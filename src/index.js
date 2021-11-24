e3e6c274f0f8d641b4a5f25c2dcd3e8322fff94b
Devin Fiachra <devinduffymeade@gmail.com>
2021-11-24 15:20:18 +0100

    changed SQUARE from a class to function component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+/*class Square extends React.Component {
     
   
     render() {
@@ -15,6 +15,15 @@ class Square extends React.Component {
          </button>
     );
   }
+}
+*/
+
+function Square(props) {
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
